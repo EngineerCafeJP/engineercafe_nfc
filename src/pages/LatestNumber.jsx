9c877d54b5d78aa5636d1ba06ece8080830f88ff
EngineerCafe A <>
2024-11-07 19:59:57 +0900

    update latestnumber logic

--- src/pages/LatestNumber.jsx
+++ src/pages/LatestNumber.jsx
@@ -1,27 +1,25 @@
 import { useState, useEffect } from 'react';
 import { db } from '../firebase';
-import {collection,query,orderBy,limit,getDocs,} from 'firebase/firestore';
+import { collection, query, getDocs, documentId, where, runTransaction } from 'firebase/firestore';
 import '../styles/LatestNumber.css';
 
 function GetLatestNumber() {
   const [latestNumber, setLatestNumber] = useState('');
 
+  const fetchLatestNumber = async () => {
+    const q = query(collection(db, 'counters'), where(documentId(), '==', 'member_number'));
+    const querySnapshot = await getDocs(q);
+    if (!querySnapshot.empty) {
+      const latestDoc = querySnapshot.docs[0];
+      const latestNum = parseInt(latestDoc.data().latest_number, 10);
+      setLatestNumber(latestNum.toString().padStart(6, '0'));
+    } else {
+      setLatestNumber('000000');
+      console.error("番号の取得に失敗しました");
+    }
+  };
+
   useEffect(() => {
-    const fetchLatestNumber = async () => {
-      const q = query(
-        collection(db, 'nfc'),
-        orderBy('number', 'desc'),
-        limit(1)
-      );
-      const querySnapshot = await getDocs(q);
-      if (!querySnapshot.empty) {
-        const latestDoc = querySnapshot.docs[0];
-        const latestNum = parseInt(latestDoc.data().number, 10) + 1;
-        setLatestNumber(latestNum.toString().padStart(6, '0'));
-      } else {
-        setLatestNumber('000001');
-      }
-    };
     fetchLatestNumber();
   }, []);
 
@@ -40,10 +38,46 @@ function GetLatestNumber() {
     }
   };
 
+  const incrementMemberNumber = (latestNumber) => {
+    // 最新の会員番号（文字列）の数値部分をインクリメント
+    let numericPart = parseInt(latestNumber, 10);
+    numericPart += 1;
+
+    // インクリメント後、ゼロ埋めして6桁の文字列に変換
+    return numericPart.toString().padStart(6, '0');
+  }
+
+  const incrementLatestNumber = async () => {
+    const q = query(collection(db, 'counters'), where(documentId(), '==', 'member_number'));
+    const querySnapshot = await getDocs(q);
+    if (!querySnapshot.empty) {
+      const latestDoc = querySnapshot.docs[0];
+      try {
+        const latestNumber = latestDoc.data().latest_number;
+        const newMemberNumber = incrementMemberNumber(latestNumber);
+        await runTransaction(db, async (transaction) => {
+          transaction.update(latestDoc.ref, { latest_number: newMemberNumber });
+        });
+        // 会員番号をインクリメント
+      } catch (error) {
+        console.error("番号のインクリメントに失敗しました:", error);
+      }
+      fetchLatestNumber();
+      alert('インクリメントしました');
+    } else {
+      console.error("番号の取得に失敗しました");
+    }
+  }
+
   return (
     <div className="latest-container">
       <h2 className="latest-title">最新の会員番号: {latestNumber}</h2>
-      <button onClick={copyClipboard} className="copy-button">コピー</button>
+      <ul className="nav-list">
+        <li className="nav-item"><button onClick={copyClipboard} className="copy-button">コピー</button>
+        </li>
+        <li className="nav-item"><button onClick={incrementLatestNumber} className="copy-button">会員番号インクリメント</button>
+        </li>
+      </ul>
     </div>
   );
 }
